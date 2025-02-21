38f3e6c3e8c4d96a152771ca95d67e8039ab06a4
jmagbojos <jantsen.magbojos@infor.com>
2025-02-22 00:34:32 +0800

    create interfaces

---
+++ RefactorThis.Persistence/Interface/IInvoiceRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace RefactorThis.Persistence.Interface
+{
+    public interface IInvoiceRepository
+    {
+        Invoice GetInvoice(string reference);
+        void SaveInvoice(Invoice invoice);
+        void Add(Invoice invoice);
+    }
+}
